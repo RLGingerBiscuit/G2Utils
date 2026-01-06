3e23d821b1801fc41f7945e65425eb068ab810ce
RLGingerBiscuit <rlgingerbiscuit@proton.me>
2026-01-06 15:14:27 +0000

    remove old item info stuff

--- G2Utils/include/G2/ItemManager.hpp
+++ G2Utils/include/G2/ItemManager.hpp
@@ -1,41 +1,12 @@
 #pragma once
 
-#include <cstdint>
-
 #include "G2/ItemHandle.hpp"
-#include "G2/PlayerHandle.hpp"
 #include "G2/Singleton.hpp"
-
-// struct ItemInfo final {
-//   explicit ItemInfo() {}
-//   explicit ItemInfo(std::string table_name, std::string name,
-//                     std::string display_name)
-//       : m_table_name(table_name), m_name(name), m_display_name(display_name)
-//       {}
-
-//   auto table_name() -> std::string & { return m_table_name; }
-//   auto table_name() const -> const std::string & { return m_table_name; }
-
-//   auto name() -> std::string & { return m_name; }
-//   auto name() const -> const std::string & { return m_name; }
-
-//   auto display_name() -> std::string & { return m_display_name; }
-//   auto display_name() const -> const std::string & { return m_display_name; }
-
-//   auto to_handle() -> ItemHandle { return ItemHandle(m_name, m_table_name); }
-
-// private:
-//   std::string m_table_name;
-//   std::string m_name;
-//   std::string m_display_name;
-// };
 
 class ItemManager final : public Singleton<ItemManager> {
   friend class Singleton<ItemManager>;
 
 public:
-  // auto get_item_info(ItemHandle handle) -> std::optional<ItemInfo>;
-
   auto is_valid_item(ItemHandle item) -> bool;
 
 protected:
--- G2Utils/src/G2/ItemManager.cpp
+++ G2Utils/src/G2/ItemManager.cpp
@@ -7,22 +7,6 @@ auto ItemManager::init_singleton() -> void {}
 
 auto ItemManager::deinit_singleton() -> void {}
 
-// auto ItemManager::get_item_info(ItemHandle handle) -> std::optional<ItemInfo>
-// {
-//   auto maybe_table = DataTableManager::get().get_table_info(
-//       DataTableHandle(handle.table_name()));
-//   if (!maybe_table.has_value())
-//     return {};
-
-//   auto table = *maybe_table;
-//   if (!table.items().contains(handle.item_name()))
-//     return {};
-
-//   // TODO: localised display name
-//   return ItemInfo(handle.table_name(), handle.item_name(),
-//   handle.item_name());
-// }
-
 auto ItemManager::is_valid_item(ItemHandle item) -> bool {
   // TODO: reduce copying
   auto table = DataTableManager::get().get_table_info(item.table_handle());
