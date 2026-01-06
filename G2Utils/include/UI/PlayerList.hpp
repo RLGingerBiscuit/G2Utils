a22bf42ec024a65fe192b7cc3d3eb8968d1ee1e6
RLGingerBiscuit <rlgingerbiscuit@proton.me>
2026-01-06 15:18:17 +0000

    don't use index for player list
    no clue why i did it in the first place tbh

--- G2Utils/include/UI/PlayerList.hpp
+++ G2Utils/include/UI/PlayerList.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cstddef>
 #include <optional>
 #include <vector>
 
@@ -12,12 +11,10 @@ public:
   auto render() -> void;
 
   auto selected_player() -> std::optional<PlayerHandle> {
-    if (m_selected_player_idx >= m_players.size())
-      return {};
-    return m_players[m_selected_player_idx];
+    return m_selected_player;
   }
 
 private:
   std::vector<PlayerHandle> m_players;
-  size_t m_selected_player_idx;
+  std::optional<PlayerHandle> m_selected_player;
 };
--- G2Utils/src/UI/PlayerList.cpp
+++ G2Utils/src/UI/PlayerList.cpp
@@ -8,9 +8,13 @@
 auto PlayerList::refresh() -> void {
   auto new_players = PlayerManager::get().get_all_players();
 
-  if (new_players.size() < m_players.size() &&
-      m_selected_player_idx >= new_players.size()) {
-    m_selected_player_idx = new_players.size() - 1;
+  if (m_selected_player.has_value()) {
+    bool found = std::any_of(
+        new_players.begin(), new_players.end(),
+        [this](auto &handle) { return handle == *m_selected_player; });
+
+    if (!found)
+      m_selected_player = {};
   }
 
   m_players = new_players;
@@ -20,20 +24,20 @@ auto PlayerList::render() -> void {
   if (ImGui::BeginListBox("Player List")) {
     defer(ImGui::EndListBox());
 
-    for (size_t i = 0; i < m_players.size(); ++i) {
-      auto &player = m_players[i];
+    for (auto &player : m_players) {
       if (ImGui::Selectable(fmt::format("{}", player).c_str(),
-                            m_selected_player_idx == i)) {
+                            m_selected_player.has_value() &&
+                                m_selected_player->id() == player.id())) {
         spdlog::info("Selected player changed to {}", player.name(),
                      player.id());
-        m_selected_player_idx = i;
+        m_selected_player = player;
       }
     }
   }
 
-  if (m_players.size() > 0) {
-    auto &selected_player = m_players[m_selected_player_idx];
-    ImGui::Text("Selected player: %s", fmt::to_string(selected_player).c_str());
+  if (m_selected_player.has_value()) {
+    ImGui::Text("Selected player: %s",
+                fmt::to_string(*m_selected_player).c_str());
   } else {
     ImGui::Text("Selected player: None");
   }
